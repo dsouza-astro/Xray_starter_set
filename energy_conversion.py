125f4223c38bda791f93167ea397742d1a407d1a
Desmond Dsouza <138887618+dsouza-astro@users.noreply.github.com>
2023-08-31 18:36:56 +0200

    Update energy_conversion.py

--- energy_conversion.py
+++ energy_conversion.py
@@ -7,8 +7,8 @@ wavelength= wavelength *10**-10
 freq= (const.c)/wavelength
 print("The frequency is: ", freq)
 
-joule_energy = 6.625E+34 *freq
+joule_energy = 6.625e+34 *freq
 
-kev_energy = joule_energy * 6.242E+15
+kev_energy = joule_energy / 1.6022e-19
 
 print("Energy is ",joule_energy, "J or ",kev_energy,"KeV")
