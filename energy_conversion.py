2b13949cd4a6ac9507f670cf2bdef81cc41b8bec
Desmond Dsouza <138887618+dsouza-astro@users.noreply.github.com>
2023-08-31 18:31:26 +0200

    Create energy_conversion.py
    code has been made

---
+++ energy_conversion.py
@@ -0,0 +1,14 @@
+# This code will convert any Armstron values to KeV for Xray fluxes. This is just for sanity reasons. 
+
+from astropy import constants as const
+
+wavelength= input("please enter the value in Amstrongs: ")
+
+freq= {const.c*10**10}/wavelength
+print("The frequency is: ", freq)
+
+joule_energy = 6.625E+34 *freq
+
+kev_energy = joule_energy * 6.242E+15
+
+print("Energy is ",joule_energy, "J or ",kev_energy,"KeV")
